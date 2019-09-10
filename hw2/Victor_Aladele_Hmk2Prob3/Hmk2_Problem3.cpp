994c07a552779dedee73ca07d19d119174503666
Victor <valad@gatech.edu>
2019-09-10 12:02:39 -0400

    Almost done with problem3. Debug segmentation fault issue

--- hw2/Victor_Aladele_Hmk2Prob3/Hmk2_Problem3.cpp
+++ hw2/Victor_Aladele_Hmk2Prob3/Hmk2_Problem3.cpp
@@ -10,6 +10,22 @@ Description:
 #include <fstream>
 #include <vector>
 
+//  Function for finding maximum sum using a dynamic programming approach
+//  Function computes max sum bottom-up, with the max sum = triangle[0][0]
+void maxPathSum(std::vector<std::vector<int>> &triangle) 
+{ 
+     // loop for moving from bottom to the top of the triangle
+     for (int i = triangle.size() - 1; i > 0; i--) 
+     { 
+        // loop for moving across row i
+        for (int j = 0; j <= i; j++) 
+        { 
+            // for each element (x), check both elements right below the number and below the number to the right (a, b)
+            // add the maximum of the two numbers max(a, b) to the number x
+            triangle[i - 1][j] += std::max(triangle[i][j], triangle[i][j + 1]); 
+        } 
+     } 
+} 
 
 int main(int argc, char** argv)
 {
@@ -30,12 +46,12 @@ int main(int argc, char** argv)
         triangle[i].resize(i + 1);
         for (int j = 0; j < i + 1; j++)
         {
-            data_triangle >> triangle[i][j];
+            data_triangle >> triangle[i][j]; // read in triangle data from inputed text file and save the values to 2D vector 'triangle'
         }
     }
     data_triangle.close();
 
-    for (int i = 0; i < triang_height; i++)
+     for (int i = 0; i < triang_height; i++)
     {
         for (int j = 0; j < triangle[i].size(); j++)
         {
@@ -44,12 +60,12 @@ int main(int argc, char** argv)
         std::cout << "\n";
     }
 
-    int max_pathSum = 0;
+    maxPathSum(triangle); // call function to compute maximum path sum
+    
     std::ofstream data_max_pathSum;
     data_max_pathSum.open("output3.txt");
-    data_max_pathSum << max_pathSum;
+    data_max_pathSum << triangle[0][0]; // write maximum path sum to file named output3.txt
     data_max_pathSum.close();
-
-
+    std::cout << triangle[0][0] << std::endl;  // test.txt stored in /home/victora/Documents
     return 0;
 }
