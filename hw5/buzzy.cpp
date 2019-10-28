538ad249cc2945e9858b9f23410d0d8c8e869ba4
Victor <valad@gatech.edu>
2019-10-28 00:50:25 -0400

    Hmk5 started. PBS script created

---
+++ hw5/buzzy.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Victor Aladele
+Class: ECE6122
+Last Date Modified: Oct 28, 2019
+Description:
+    A distributed MPI program to simulate the docking of 
+    spacehips (Yellow Jackets) with the mothership (Buzzy)
+*/
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
