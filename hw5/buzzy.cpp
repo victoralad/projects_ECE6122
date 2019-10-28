031544467630e0f5d11eb6fd0b19e163669a834c
Victor <valad@gatech.edu>
2019-10-28 14:18:31 -0400

    Able to read in data properly now

--- hw5/buzzy.cpp
+++ hw5/buzzy.cpp
@@ -13,7 +13,11 @@ Description:
 #include <mpi.h>
 
 #define  MASTER  0
+#define SHIPMASS 10000;
 
+int timeOut; 
+
+double maxThrust;
 double shipSpeed;
 std::vector<double> shipPos(3);
 std::vector<double> shipSpeedDir(3);
@@ -22,24 +26,42 @@ void readInputData();
 
 void readInputData() 
 {
-    int timeOut; 
-    double maxThrust;
-    double allShipSpeed;
-    std::vector<double> allShipPos(3);
-    std::vector<double> allShipSpeedDir(3);
+    std::vector<double> yelJacSpeed(7);
+    std::vector<std::vector<double> > yelJacPos(7, std::vector<double> (3, 0));
+    std::vector<std::vector<double> > yelJacSpeedDir(7, std::vector<double> (3, 0));
 
     std::ifstream input;
     input.open("in.dat");
     input >> timeOut;
     input >> maxThrust;
-    for (int i = 0; i < 3; ++i) 
+
+    // read in data for just Buzzy
+    for (int k = 0; k < 3; ++k) 
     {
-        input >> allShipPos[i]
+        input >> shipPos[k];
     }
-    input >> allShipSpeed;
-    for (int i = 0; i < 3; ++i) 
+
+    input >> shipSpeed;
+
+    for (int k = 0; k < 3; ++k) 
     {
-        input >> allShipSpeedDir[i]
+        input >> shipSpeedDir[k];
+    }
+
+    // read in data for the 7 Yellow Jackets 
+    for (int i = 0; i < 7; ++i)
+    {
+        for (int j = 0; j < 3; ++j) 
+        {
+            input >> yelJacPos[i][j];
+        }
+
+        input >> yelJacSpeed[i];
+
+        for (int j = 0; j < 3; ++j) 
+        {
+            input >> yelJacSpeedDir[i][j];
+        }
     }
     input.close();
 }
@@ -48,7 +70,7 @@ int main(int argc, char *argv[])
 {
 
     
-    int numTasks, taskId, len;
+    int numTasks, rank, len;
     char hostname[MPI_MAX_PROCESSOR_NAME];
 
     
@@ -56,20 +78,44 @@ int main(int argc, char *argv[])
     
 
     MPI_Comm_size(MPI_COMM_WORLD, &numTasks);
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskId);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Get_processor_name(hostname, &len);
 
     
-    if (taskId == MASTER)
+    if (rank == MASTER)
     {
-        printf("MASTER: Number of MPI tasks is: %d\n", numTasks);
         readInputData();
+        std::cout << "maxThrust" << rank << ": " << maxThrust << std::endl;
+        // for (int i = 0; i < 7; ++i)
+        // {
+        //     cout << "Rank " << rank << " sending to rank " << rank + 1 << endl;
+        //     rc = MPI_Send(  buf,            // void* data
+        //                     sizeof(buf),    // int count
+        //                     MPI_CHAR,       // MPI_Datatype datatype
+        //                     rank + 1,       // int destination
+        //                     0,              // int Message tag ID
+        //                     MPI_COMM_WORLD);// MPI_Comm communicator
+        //     if (rc != MPI_SUCCESS)
+        //     {
+        //         cout << "Rank " << rank
+        //             << " send failed, rc " << rc << endl;
+        //         MPI_Finalize();
+        //         exit(1);
+        //     }
+        // }
 
+    }
+
+    for (int i = 1; i < 8; ++i)
+    {
+        if (rank == i)
+        {
+            // std::cout << "maxThrust" << rank << ": " << maxThrust << std::endl;
+        }
     }
 
     while (timeOut > 0)
     {
-
         timeOut--;
     }
 
