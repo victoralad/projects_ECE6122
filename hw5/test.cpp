a67bc6ab1fdc7c5b217838996d546f3a1f3a2b86
Victor <valad@gatech.edu>
2019-10-31 14:29:25 -0400

    Better output, just without scientific notation

--- hw5/test.cpp
+++ hw5/test.cpp
@@ -11,14 +11,18 @@
 
 #define SHIPMASS 10000;
 
+int numtasks, rank, rc;
+const int root = 0;
+
 int timeOut; 
 double maxThrust;
-double shipSpeed;
-int shipStatus = 1; // set all ships status to active
-int recvAllShipStatus[8] = {0};
+int shipStatus = 1;                 // set all ships status to active
 double allShipInfo[56] = {0};
-double recvAllShipInfo[56] = {0};
-double shipInfo[7] = {0};
+double shipVel[4] = {0};
+double recvAllshipVel[32] = {0};
+double force[3] = {0};
+double shipInfo[8] = {0};           // rankID, status, x, y, z, Fx , Fy , Fz
+double recvAllShipInfo[64] = {0};   // rankID, status, x, y, z, Fx , Fy , Fz  * 8
 
 void CalculateBuzzyXYZ();
 
@@ -29,22 +33,26 @@ void readInputData();
 void CalculateBuzzyXYZ()
 {
     // calculate new position of Buzzy using Newton's equation s1 = s0 + v*t
-    shipInfo[0] += shipInfo[3] * shipInfo[4]; // x
-    shipInfo[1] += shipInfo[3] * shipInfo[5]; // y
-    shipInfo[2] += shipInfo[3] * shipInfo[6]; // z
+    shipInfo[2] += shipVel[0] * shipVel[1]; // y
+    shipInfo[3] += shipVel[0] * shipVel[2]; // z
+    shipInfo[4] += shipVel[0] * shipVel[3]; // z
 }
 
 void CalculateYellowJacketXYZ()
 {
-    std::cout << "hey" << std::endl;
+    shipInfo[1] = shipStatus;
+    for (int i = 2; i < 5; ++i)
+    {
+        shipInfo[i] = allShipInfo[7*rank + i];
+    }
+    for (int i = 5; i < 8; ++i)
+    {
+        shipInfo[i] = force[i - 5];
+    }
 }
 
 void readInputData() 
 {
-    // std::vector<double> yelJacSpeed(7);
-    // std::vector<std::vector<double> > yelJacPos(numtasks, std::vector<double> (3, 0));
-    // std::vector<std::vector<double> > yelJacSpeedDir(numtasks, std::vector<double> (3, 0));
-
     std::ifstream input;
     input.open("in.dat");
     input >> timeOut;
@@ -61,8 +69,6 @@ void readInputData()
 
 int main(int argc, char**argv)
 {
-    int numtasks, rank, rc;
-    const int root = 0;
 
     rc = MPI_Init(&argc, &argv);
 
@@ -76,7 +82,7 @@ int main(int argc, char**argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // Seed the random number generator to get different results each time
-    srand(rank);
+    // srand(rank);
     if (rank == 0)
     {
         // Load in.dat file
@@ -90,9 +96,22 @@ int main(int argc, char**argv)
     MPI_Bcast(&maxThrust, 1, MPI_DOUBLE, root, MPI_COMM_WORLD);
     MPI_Bcast(allShipInfo, 56, MPI_DOUBLE, root, MPI_COMM_WORLD);
 
-    for (int i = 0; i < 7; ++i)
+    // set the initial ship velocity for all ships from allShipInfo
+    for (int i = 0; i < 4; ++i)
     {
-        shipInfo[i] = allShipInfo[7*rank + i];
+        shipVel[i] = allShipInfo[i + 3];
+    }
+
+    // set the initial values to be printed to the screen by Buzzy
+    shipInfo[0] = rank;
+    shipInfo[1] = shipStatus;
+    for (int i = 2; i < 5; ++i)
+    {
+        shipInfo[i] = allShipInfo[7*rank + i - 2]; 
+    }
+    for (int i = 5; i < 8; ++i)
+    {
+        shipInfo[i] = force[i - 5];
     }
 
     // Loop through the number of time steps
@@ -100,39 +119,33 @@ int main(int argc, char**argv)
     {
         if (rank == 0)
         {
+            MPI_Allgather(shipVel, 4, MPI_DOUBLE, recvAllshipVel, 4, MPI_DOUBLE, MPI_COMM_WORLD);
+
             // Calculate Buzzy new location
             CalculateBuzzyXYZ();
 
             // Share information with other yellow jackets
-            MPI_Allgather(&shipStatus, 1, MPI_INT, recvAllShipStatus, 1, MPI_INT, MPI_COMM_WORLD);
-            MPI_Allgather(shipInfo, 7, MPI_DOUBLE, recvAllShipInfo, 7, MPI_DOUBLE, MPI_COMM_WORLD);
+            MPI_Allgather(shipInfo, 8, MPI_DOUBLE, recvAllShipInfo, 8, MPI_DOUBLE, MPI_COMM_WORLD);
 
             // Output all ship info to console:  rankID, status, x, y, z, F x , F y , F z
-            std::cout << "----------------------- Round " << round << " -----------------------" << std::endl;
-            for (int i = 0; i < 56; ++i)
+            std::cout << "----------------------- Round " << round << " -------rankID, status, x, y, z, Fx , Fy , Fz -----------------------" << std::endl;
+            for (int i = 0; i < numtasks; ++i)
             {
-                std::cout << recvAllShipInfo[i] << " ";
-                if ((i + 1) % 7 == 0)
+                for (int j = 0; j < 8; ++j)
                 {
-                    std::cout << std::endl;
+                    std::cout << recvAllShipInfo[8*i + j] << " ";
                 }
+                std::cout << std::endl;
             }
         }
         else
         {
+            MPI_Allgather(shipVel, 4, MPI_DOUBLE, recvAllshipVel, 4, MPI_DOUBLE, MPI_COMM_WORLD);
             // Calculate yellow jacket new location
             // CalculateYellowJacketXYZ();
-            shipStatus = rank;
-            MPI_Allgather(&shipStatus, 1, MPI_INT, recvAllShipStatus, 1, MPI_INT, MPI_COMM_WORLD);
-            
-            // for (int i = 0; i < numtasks; ++i)
-            // {
-            //     std::cout << recvAllShipStatus[i] << " ";
-            // }
-            // std::cout << std::endl;
 
            
-            MPI_Allgather(shipInfo, 7, MPI_DOUBLE, recvAllShipInfo, 7, MPI_DOUBLE, MPI_COMM_WORLD);
+            MPI_Allgather(shipInfo, 8, MPI_DOUBLE, recvAllShipInfo, 8, MPI_DOUBLE, MPI_COMM_WORLD);
             
         }
     }
