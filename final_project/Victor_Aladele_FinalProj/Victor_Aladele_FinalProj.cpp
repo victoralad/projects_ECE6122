ee9e0c5aeca66494910e633cbe7c51634f5fade6
Victor <valad@gatech.edu>
2019-11-17 12:58:41 -0500

    Drawn football field

--- final_project/Victor_Aladele_FinalProj/Victor_Aladele_FinalProj.cpp
+++ final_project/Victor_Aladele_FinalProj/Victor_Aladele_FinalProj.cpp
@@ -16,12 +16,16 @@ Description:
 #define ESC 27
 
 GLfloat light0_ambient[] = {0.2, 0.2, 0.2, 1.0};
+GLfloat light0_diffuse[] = {0.0, 0.0, 0.0, 0.0};
+GLfloat light0_specular[] = {0.0, 0.0, 0.0, 0.0};
+GLfloat light1_ambient[] = {0.0, 0.0, 0.0, 0.0};
 GLfloat light1_diffuse[] = {0.5, 0.5, 0.5, 1.0};
 GLfloat light1_specular[] = {0.3, 0.3, 0.3, 1.0};
 GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
 GLfloat mat_shininess[] = {50.0};
-GLfloat light1_position[] = {-5.0, -5.0, 8.0};
+GLfloat light1_position[] = {5.0, 5.0, 8.0};
 GLdouble width = 0.75, depth = 0.75, height = 1.0;
+bool light0Enable = true, light1Enable = true;
 
 void init(void)
 {
@@ -30,9 +34,15 @@ void init(void)
 
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
     glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light1_ambient);
     glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
     glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);
+
     glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
 
     glEnable(GL_LIGHTING);
@@ -58,11 +68,11 @@ void init(void)
 //----------------------------------------------------------------------
 
 // Camera position
-float x = 4.0, y = -5.0, z = 10; // initially 5 units south of origin
+float x = 15.0, y = 0.0, z = 50; // initially 5 units south of origin
 float deltaMove = 0.0; // initially camera doesn't move
 
 // Camera direction
-float lx = 4.0, ly = 4.0, lz = 0.0; 
+float lx = 0.0, ly = 0.0, lz = 0.0; 
 float angle = 0.0; // angle of rotation for the camera direction
 
 //----------------------------------------------------------------------
@@ -79,7 +89,7 @@ void changeSize(int w, int h)
     float ratio = ((float)w) / ((float)h); // window aspect ratio
     glMatrixMode(GL_PROJECTION); // projection matrix is active
     glLoadIdentity(); // reset the projection
-    gluPerspective(43, ratio, 0.1, 30.0); // perspective transformation
+    gluPerspective(105, ratio, 0.1, 100.0); // perspective transformation
     glMatrixMode(GL_MODELVIEW); // return to modelview mode
     glViewport(0, 0, w, h); // set viewport (drawing area) to entire window
 }
@@ -117,7 +127,7 @@ void renderScene(void)
     int i, j;
 
      // Clear color and depth buffers
-    glClearColor(0.32, 0.29, 0.27, 1.0); // (0.196078, 0.6, 0.8, 1.0); // background color is blue
+    glClearColor(0.7, 0.8, 1.0, 1.0); // background color is blue
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Reset transformations
@@ -131,15 +141,15 @@ void renderScene(void)
         0.0, 0.0, 1.0);
 
     // draw football field
-    // glColor3f(0.196078, 0.6, 0.8);
-    // glPushMatrix();
-    //     glBegin(GL_QUADS);
-    //         glVertex3f(-24.375, -55.0, 0.0);
-    //         glVertex3f(-24.375, 55.0, 0.0);
-    //         glVertex3f(24.375, 55.0, 0.0);
-    //         glVertex3f(24.375, -55.0, 0.0);
-    //     glEnd();
-    // glPopMatrix();
+    glColor3f(0.1, 0.6, 0.1);
+    glPushMatrix();
+        glBegin(GL_QUADS);
+            glVertex3f(-24.375, -55.0, 0.0);
+            glVertex3f(-24.375, 55.0, 0.0);
+            glVertex3f(24.375, 55.0, 0.0);
+            glVertex3f(24.375, -55.0, 0.0);
+        glEnd();
+    glPopMatrix();
 
 
     glutSwapBuffers(); // Make it all visible
